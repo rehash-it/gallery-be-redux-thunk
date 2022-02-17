a1f8e3411a0e4957368191829fc75f02739f8b8b
Dagi <dagi.zewdu.dz@gmail.com>
2022-02-18 00:38:17 +0300

    Modified search api filte functionality added

--- routes/index.js
+++ routes/index.js
@@ -98,63 +98,30 @@ module.exports = function (app) {
 
   app.use("/api", router.post("/auth", asyncMiddleware(authController.auth)));
 
-  app.use(
-    "/api",
-    router.get("/me/:id", asyncMiddleware(userController.getUser))
-  );
+  app.use("/api", router.get("/me/:id", asyncMiddleware(userController.getUser)));
+
+  app.use("/api", router.post("/user", asyncMiddleware(userController.createUser)));
+  app.use("/api", router.get("/user", asyncMiddleware(userController.getUsers)));
+  app.use("/api", router.get("/user/:id", asyncMiddleware(userController.getUserById)));
+  app.use("/api", router.put("/user/:id", asyncMiddleware(userController.updateUser)));
+  app.use("/api", router.patch("/user/:id", asyncMiddleware(userController.updateUser)));
+  app.use("/api", router.delete("/user/:id", asyncMiddleware(userController.deleteUser)));
 
   app.use(
     "/api",
-    router.post("/user", asyncMiddleware(userController.createUser))
+    router.post("/contactInfo", asyncMiddleware(contactInfoController.createContactInfo))
   );
   app.use(
     "/api",
-    router.get("/user", asyncMiddleware(userController.getUsers))
+    router.get("/contactInfo", asyncMiddleware(contactInfoController.getContactInfo))
   );
   app.use(
     "/api",
-    router.get("/user/:id", asyncMiddleware(userController.getUserById))
+    router.put("/contactInfo/:id", asyncMiddleware(contactInfoController.updateContactInfo))
   );
   app.use(
     "/api",
-    router.put("/user/:id", asyncMiddleware(userController.updateUser))
-  );
-  app.use(
-    "/api",
-    router.patch("/user/:id", asyncMiddleware(userController.updateUser))
-  );
-  app.use(
-    "/api",
-    router.delete("/user/:id", asyncMiddleware(userController.deleteUser))
-  );
-
-  app.use(
-    "/api",
-    router.post(
-      "/contactInfo",
-      asyncMiddleware(contactInfoController.createContactInfo)
-    )
-  );
-  app.use(
-    "/api",
-    router.get(
-      "/contactInfo",
-      asyncMiddleware(contactInfoController.getContactInfo)
-    )
-  );
-  app.use(
-    "/api",
-    router.put(
-      "/contactInfo/:id",
-      asyncMiddleware(contactInfoController.updateContactInfo)
-    )
-  );
-  app.use(
-    "/api",
-    router.delete(
-      "/contactInfo/:id",
-      asyncMiddleware(contactInfoController.deleteContactInfo)
-    )
+    router.delete("/contactInfo/:id", asyncMiddleware(contactInfoController.deleteContactInfo))
   );
 
   app.use(
@@ -166,10 +133,7 @@ module.exports = function (app) {
   );
   app.use(
     "/api",
-    router.get(
-      "/galleryCategory",
-      asyncMiddleware(galleryCategoryController.getGalleryCategory)
-    )
+    router.get("/galleryCategory", asyncMiddleware(galleryCategoryController.getGalleryCategory))
   );
   app.use(
     "/api",
@@ -193,183 +157,66 @@ module.exports = function (app) {
     )
   );
 
+  app.use("/api", router.post("/gallery", asyncMiddleware(galleryController.createGallery)));
+  app.use("/api", router.post("/gallery/like/:id", asyncMiddleware(galleryController.updateLike)));
   app.use(
     "/api",
-    router.post("/gallery", asyncMiddleware(galleryController.createGallery))
+    router.post("/gallery/unlike/:id", asyncMiddleware(galleryController.updateUnLike))
+  );
+
+  app.use("/api", router.post("/gallery/view/:id", asyncMiddleware(galleryController.updateView)));
+  app.use("/api", router.get("/gallery", asyncMiddleware(galleryController.getGallery)));
+  app.use("/api", router.get("/galleries", asyncMiddleware(galleryController.getGalleries)));
+  app.use(
+    "/api",
+    router.get("/galleryByCategory/:id", asyncMiddleware(galleryController.getGalleryByCategory))
+  );
+  app.use("/api", router.get("/gallery/:id", asyncMiddleware(galleryController.getGalleryById)));
+  app.use("/api", router.put("/gallery/:id", asyncMiddleware(galleryController.updateGallery)));
+  app.use("/api", router.delete("/gallery/:id", asyncMiddleware(galleryController.deleteGallery)));
+  app.use(
+    "/api",
+    router.put("/gallery/status/:data", asyncMiddleware(galleryController.updateGalleryStatus))
   );
   app.use(
     "/api",
-    router.post(
-      "/gallery/like/:id",
-      asyncMiddleware(galleryController.updateLike)
-    )
+    router.get("/galleryreq/", asyncMiddleware(galleryController.getGalleryByReqStatus))
   );
-  app.use(
-    "/api",
-    router.post(
-      "/gallery/unlike/:id",
-      asyncMiddleware(galleryController.updateUnLike)
-    )
-  );
+
+  app.use("/api", router.post("/lookup", asyncMiddleware(lookupController.createLookup)));
+  app.use("/api", router.get("/lookup", asyncMiddleware(lookupController.getLookup)));
+  app.use("/api", router.get("/lookup/:type", asyncMiddleware(lookupController.getLookupByType)));
+  app.use("/api", router.put("/lookup/:id", asyncMiddleware(lookupController.updateLookup)));
+  app.use("/api", router.delete("/lookup/:id", asyncMiddleware(lookupController.deleteLookup)));
+  app.use("/api", router.get("/lookupid/:id", asyncMiddleware(lookupController.getLookupById)));
+
+  app.use("/api", router.post("/event", asyncMiddleware(eventController.createEvent)));
+  app.use("/api", router.get("/event", asyncMiddleware(eventController.getEvent)));
+  app.use("/api", router.put("/event/:id", asyncMiddleware(eventController.updateEvent)));
+  app.use("/api", router.delete("/event/:id", asyncMiddleware(eventController.deleteEvent)));
+  app.use("/api", router.get("/event/:id", asyncMiddleware(eventController.getEventById)));
+
+  app.use("/api", router.post("/sponsor", asyncMiddleware(sponsorController.createSponsor)));
+  app.use("/api", router.get("/sponsor", asyncMiddleware(sponsorController.getSponsor)));
+  app.use("/api", router.get("/sponsor/:id", asyncMiddleware(sponsorController.getSponsorById)));
+  app.use("/api", router.put("/sponsor/:id", asyncMiddleware(sponsorController.updateSponsor)));
+  app.use("/api", router.delete("/sponsor/:id", asyncMiddleware(sponsorController.deleteSponsor)));
 
   app.use(
     "/api",
-    router.post(
-      "/gallery/view/:id",
-      asyncMiddleware(galleryController.updateView)
-    )
+    router.post("/advertisement", asyncMiddleware(advertisementController.createAdvertisement))
   );
   app.use(
     "/api",
-    router.get("/gallery", asyncMiddleware(galleryController.getGallery))
+    router.get("/advertisement", asyncMiddleware(advertisementController.getAdvertisement))
   );
   app.use(
     "/api",
-    router.get("/galleries", asyncMiddleware(galleryController.getGalleries))
+    router.get("/advertisement/filtered", asyncMiddleware(advertisementController.getAdverts))
   );
   app.use(
     "/api",
-    router.get(
-      "/galleryByCategory/:id",
-      asyncMiddleware(galleryController.getGalleryByCategory)
-    )
-  );
-  app.use(
-    "/api",
-    router.get(
-      "/gallery/:id",
-      asyncMiddleware(galleryController.getGalleryById)
-    )
-  );
-  app.use(
-    "/api",
-    router.put("/gallery/:id", asyncMiddleware(galleryController.updateGallery))
-  );
-  app.use(
-    "/api",
-    router.delete(
-      "/gallery/:id",
-      asyncMiddleware(galleryController.deleteGallery)
-    )
-  );
-  app.use(
-    "/api",
-    router.put(
-      "/gallery/status/:data",
-      asyncMiddleware(galleryController.updateGalleryStatus)
-    )
-  );
-  app.use(
-    "/api",
-    router.get(
-      "/galleryreq/",
-      asyncMiddleware(galleryController.getGalleryByReqStatus)
-    )
-  );
-
-  app.use(
-    "/api",
-    router.post("/lookup", asyncMiddleware(lookupController.createLookup))
-  );
-  app.use(
-    "/api",
-    router.get("/lookup", asyncMiddleware(lookupController.getLookup))
-  );
-  app.use(
-    "/api",
-    router.get(
-      "/lookup/:type",
-      asyncMiddleware(lookupController.getLookupByType)
-    )
-  );
-  app.use(
-    "/api",
-    router.put("/lookup/:id", asyncMiddleware(lookupController.updateLookup))
-  );
-  app.use(
-    "/api",
-    router.delete("/lookup/:id", asyncMiddleware(lookupController.deleteLookup))
-  );
-  app.use(
-    "/api",
-    router.get("/lookupid/:id", asyncMiddleware(lookupController.getLookupById))
-  );
-
-  app.use(
-    "/api",
-    router.post("/event", asyncMiddleware(eventController.createEvent))
-  );
-  app.use(
-    "/api",
-    router.get("/event", asyncMiddleware(eventController.getEvent))
-  );
-  app.use(
-    "/api",
-    router.put("/event/:id", asyncMiddleware(eventController.updateEvent))
-  );
-  app.use(
-    "/api",
-    router.delete("/event/:id", asyncMiddleware(eventController.deleteEvent))
-  );
-  app.use(
-    "/api",
-    router.get("/event/:id", asyncMiddleware(eventController.getEventById))
-  );
-
-  app.use(
-    "/api",
-    router.post("/sponsor", asyncMiddleware(sponsorController.createSponsor))
-  );
-  app.use(
-    "/api",
-    router.get("/sponsor", asyncMiddleware(sponsorController.getSponsor))
-  );
-  app.use(
-    "/api",
-    router.get(
-      "/sponsor/:id",
-      asyncMiddleware(sponsorController.getSponsorById)
-    )
-  );
-  app.use(
-    "/api",
-    router.put("/sponsor/:id", asyncMiddleware(sponsorController.updateSponsor))
-  );
-  app.use(
-    "/api",
-    router.delete(
-      "/sponsor/:id",
-      asyncMiddleware(sponsorController.deleteSponsor)
-    )
-  );
-
-  app.use(
-    "/api",
-    router.post(
-      "/advertisement",
-      asyncMiddleware(advertisementController.createAdvertisement)
-    )
-  );
-  app.use(
-    "/api",
-    router.get(
-      "/advertisement",
-      asyncMiddleware(advertisementController.getAdvertisement)
-    )
-  );
-  app.use(
-    "/api",
-    router.get(
-      "/advertisement/filtered",
-      asyncMiddleware(advertisementController.getAdverts)
-    )
-  );
-  app.use(
-    "/api",
-    router.get(
-      "/advertisement/:id",
-      asyncMiddleware(advertisementController.getAdvertisementById)
-    )
+    router.get("/advertisement/:id", asyncMiddleware(advertisementController.getAdvertisementById))
   );
   app.use(
     "/api",
@@ -380,10 +227,7 @@ module.exports = function (app) {
   );
   app.use(
     "/api",
-    router.put(
-      "/advertisement/:id",
-      asyncMiddleware(advertisementController.updateAdvertisement)
-    )
+    router.put("/advertisement/:id", asyncMiddleware(advertisementController.updateAdvertisement))
   );
   app.use(
     "/api",
@@ -394,25 +238,16 @@ module.exports = function (app) {
   );
   app.use(
     "/api",
-    router.get(
-      "/advertisementreq/",
-      asyncMiddleware(advertisementController.getAdsByReqStatus)
-    )
+    router.get("/advertisementreq/", asyncMiddleware(advertisementController.getAdsByReqStatus))
   );
 
   app.use(
     "/api",
-    router.post(
-      "/upcomingevent",
-      asyncMiddleware(upcomingeventsController.createUpcomingEvent)
-    )
+    router.post("/upcomingevent", asyncMiddleware(upcomingeventsController.createUpcomingEvent))
   );
   app.use(
     "/api",
-    router.get(
-      "/upcomingevent",
-      asyncMiddleware(upcomingeventsController.getUpcomingEvent)
-    )
+    router.get("/upcomingevent", asyncMiddleware(upcomingeventsController.getUpcomingEvent))
   );
   app.use(
     "/api",
@@ -423,10 +258,7 @@ module.exports = function (app) {
   );
   app.use(
     "/api",
-    router.get(
-      "/upcomingevent/:id",
-      asyncMiddleware(upcomingeventsController.getOneEvent)
-    )
+    router.get("/upcomingevent/:id", asyncMiddleware(upcomingeventsController.getOneEvent))
   );
   app.use(
     "/api",
@@ -445,94 +277,34 @@ module.exports = function (app) {
     )
   );
 
-  app.use(
-    "/api",
-    router.post("/faq", asyncMiddleware(faqController.createFaq))
-  );
+  app.use("/api", router.post("/faq", asyncMiddleware(faqController.createFaq)));
   app.use("/api", router.get("/faq", asyncMiddleware(faqController.getFaq)));
-  app.use(
-    "/api",
-    router.get("/faq/:id", asyncMiddleware(faqController.getFaqById))
-  );
-  app.use(
-    "/api",
-    router.get("/faq/:type", asyncMiddleware(faqController.getFAQByQuestion))
-  );
-  app.use(
-    "/api",
-    router.put("/faq/:id", asyncMiddleware(faqController.updateFaq))
-  );
-  app.use(
-    "/api",
-    router.delete("/faq/:id", [auth], asyncMiddleware(faqController.deleteFaq))
-  );
+  app.use("/api", router.get("/faq/:id", asyncMiddleware(faqController.getFaqById)));
+  app.use("/api", router.get("/faq/:type", asyncMiddleware(faqController.getFAQByQuestion)));
+  app.use("/api", router.put("/faq/:id", asyncMiddleware(faqController.updateFaq)));
+  app.use("/api", router.delete("/faq/:id", [auth], asyncMiddleware(faqController.deleteFaq)));
 
-  app.use(
-    "/api",
-    router.post("/signup", asyncMiddleware(authController.signUp))
-  );
-  app.use(
-    "/api",
-    router.post("/signin", asyncMiddleware(authController.signin))
-  );
-  app.use(
-    "/api",
-    router.get("/checkuser/:id", asyncMiddleware(authController.checkUser))
-  );
+  app.use("/api", router.post("/signup", asyncMiddleware(authController.signUp)));
+  app.use("/api", router.post("/signin", asyncMiddleware(authController.signin)));
+  app.use("/api", router.get("/checkuser/:id", asyncMiddleware(authController.checkUser)));
 
-  app.use(
-    "/api",
-    router.post("/comments", asyncMiddleware(Comment.createComment))
-  );
-  app.use(
-    "/api",
-    router.get("/comments", asyncMiddleware(Comment.getComments))
-  );
-  app.use(
-    "/api",
-    router.get(
-      "/comments/:gallery_id",
-      asyncMiddleware(Comment.galleryComments)
-    )
-  );
-  app.use(
-    "/api",
-    router.put("/comments/:id", asyncMiddleware(Comment.updateComment))
-  );
-  app.use(
-    "/api",
-    router.delete("/comments/:id", asyncMiddleware(Comment.deleteComment))
-  );
-  app.use(
-    "/api",
-    router.get("/usercomments/:user_id", asyncMiddleware(Comment.userComments))
-  );
+  app.use("/api", router.post("/comments", asyncMiddleware(Comment.createComment)));
+  app.use("/api", router.get("/comments", asyncMiddleware(Comment.getComments)));
+  app.use("/api", router.get("/comments/:gallery_id", asyncMiddleware(Comment.galleryComments)));
+  app.use("/api", router.put("/comments/:id", asyncMiddleware(Comment.updateComment)));
+  app.use("/api", router.delete("/comments/:id", asyncMiddleware(Comment.deleteComment)));
+  app.use("/api", router.get("/usercomments/:user_id", asyncMiddleware(Comment.userComments)));
 
-  app.use(
-    "/api",
-    router.get("/reports", asyncMiddleware(Report.getAllReports))
-  ); //all reports
-  app.use(
-    "/api",
-    router.get("/user_reports/:user_id", asyncMiddleware(Report.getUserReports))
-  ); //user reports
-  app.use(
-    "/api",
-    router.get("/reports/:gallery_id", asyncMiddleware(Report.galleryReports))
-  ); //gallery reports
-  app.use(
-    "/api",
-    router.get("/report/:gallery_id", asyncMiddleware(Report.checkReport))
-  ); //checks if the user is reported before or not
+  app.use("/api", router.get("/reports", asyncMiddleware(Report.getAllReports))); //all reports
+  app.use("/api", router.get("/user_reports/:user_id", asyncMiddleware(Report.getUserReports))); //user reports
+  app.use("/api", router.get("/reports/:gallery_id", asyncMiddleware(Report.galleryReports))); //gallery reports
+  app.use("/api", router.get("/report/:gallery_id", asyncMiddleware(Report.checkReport))); //checks if the user is reported before or not
   app.use("/api", router.post("/report", asyncMiddleware(Report.createReport)));
 
-  app.use(
-    "/api",
-    router.get("/search/:index", asyncMiddleware(searchController.search))
-  );
-  app.use(express.static(path.join(__dirname, "../../client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
+  app.use("/api", router.get("/search/:index", asyncMiddleware(searchController.search)));
+  // app.use(express.static(path.join(__dirname, "../../client/build")));
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "build", "index.html"));
+  // });
   app.use(error);
 };
